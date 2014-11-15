db35aa4eb6d1e4ce380224c93517272079c8e590
Yundi Huang <kenkaka.hyd@gmail.com>
2014-11-15 17:43:01 -0500

    Initial Commit

---
+++ KHSwipeViewPresenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KHSwipeViewPresenter
+//
+//  Created by Ken Huang on 2014-11-15.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KHSwipeViewPresenterTests/KHSwipeViewPresenterTests.m
@@ -0,0 +1,40 @@
+//
+//  KHSwipeViewPresenterTests.m
+//  KHSwipeViewPresenterTests
+//
+//  Created by Ken Huang on 2014-11-15.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface KHSwipeViewPresenterTests : XCTestCase
+
+@end
+
+@implementation KHSwipeViewPresenterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
